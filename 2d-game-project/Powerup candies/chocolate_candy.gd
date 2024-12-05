bda3b6e30b818b5aa11f2a33a427241460394ba2
Mabel <ma.b.el.202423@gmail.com>
2024-12-04 21:22:59 -0500

    Added the chocolate candy for restoring a life

---
+++ 2d-game-project/Powerup candies/chocolate_candy.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var speed: float = 200.0  # Speed at which the candy falls
+
+# Called when the node enters the scene tree for the first time
+func _ready() -> void:
+	# Start falling immediately when spawned
+	set_process(true)
+
+# Called every frame
+func _process(delta: float) -> void:
+	# Move the candy downwards
+	position.y += speed * delta #moves the candy downward
+	
+	if position.y > get_viewport_rect().size.y:
+		queue_free()  # Remove candy
+
+# Function to handle collisions with the alien
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("alien"):
+		get_tree().current_scene.restore_life() #retores a life when it comes into contact with the alien
+		queue_free()  # Remove the chocolate after collection
--- 2d-game-project/heart_greying.gd
+++ 2d-game-project/heart_greying.gd
@@ -3,3 +3,7 @@ extends Node2D
 # Function to switch to greyscale
 func grey_scale():
 	modulate = Color(0.5, 0.5, 0.5, 1) # Dims color for grayscale effect
+	
+# Function to restore the greyscale
+func restore_scale():
+	modulate = Color(1, 1, 1, 1) # Restores the grayscale effect
