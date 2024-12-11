2754099abb5ca58360d451a09bb72670f2215cf4
Juliana Al-Ola <alolajuliana@gmail.com>
2024-12-11 00:51:10 -0500

    added freeze candy powerup

--- 2d-game-project/frozen_candy.gd
+++ 2d-game-project/frozen_candy.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @export var speed: float = 200.0  # Speed at which the candy falls
+@export var fall_speed_multiplier = 1.0
 
-# Called when the node enters the scene tree for the first time
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Start falling immediately when spawned
 	set_process(true)
@@ -12,6 +13,7 @@ func _process(delta: float) -> void:
 	# Move the candy downwards
 	position.y += speed * delta #moves the candy downward
 	
+	# Check if the candy reaches the bottom of the screen
 	if position.y > get_viewport_rect().size.y:
 		Globals.powerupOnScreen = false
 		queue_free()  # Remove candy
@@ -19,6 +21,7 @@ func _process(delta: float) -> void:
 # Function to handle collisions with the alien
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("alien"):
-		get_tree().current_scene.restore_life() # Restores a life when it comes into contact with the alien
+		# Activate the frozen effect in the main script
+		get_parent().call("activate_frozen_mode")
 		Globals.powerupOnScreen = false 
-		queue_free()  # Remove the chocolate after collection
+		queue_free()
