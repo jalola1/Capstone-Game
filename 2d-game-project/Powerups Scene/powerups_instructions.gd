69dc1d7e00170bea48fc88667c223897b41f065b
Mabel <ma.b.el.202423@gmail.com>
2024-12-11 00:47:10 -0500

    Added new landing image, Added and edited powerup sounds and gameover sound, Added powerups instructions page

--- 2d-game-project/Powerups Scene/powerups_instructions.gd
+++ 2d-game-project/Powerups Scene/powerups_instructions.gd
@@ -4,8 +4,9 @@ func _ready() -> void:
 	if $IntroMusicContinued and !$IntroMusicContinued.playing:
 		$IntroMusicContinued.play()
 
-func _on_button_pressed():
-	$NextButtonSound.play()
-
+func _on_startbutton_pressed():
+	$EnterGameSound.play()
+	
+	
 func _on_next_button_sound_finished() -> void:
-	get_tree().change_scene_to_file("res://Powerups Scene/powerups_instructions.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
